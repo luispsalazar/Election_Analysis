108a4966cdd20899da1189e079321f02ffe8bd42
Luis Salazar <luispsalazar@yahoo.com>
2022-02-11 17:24:52 -0600

    finished sections 3.5.5

--- PyPoll.py
+++ PyPoll.py
@@ -56,8 +56,47 @@ with open(file_to_load) as election_data:
         candidate_votes[candidate_name] += 1
 
 # Print the candidate list.
-print(candidate_votes)
+    print(candidate_votes)
         
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Print the total votes
 print(f"Total votes = ", total_votes)
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+
+# Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    
+    # Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    
+    # Print the candidate name and percentage of votes.
+    #print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+
+# Determine winning vote count and candidate
+# Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+    
+    # If true then set winning_count = votes and winning_percent = vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+    
+    # And, set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+#  To do: print out the winning candidate, vote count and percentage to terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
